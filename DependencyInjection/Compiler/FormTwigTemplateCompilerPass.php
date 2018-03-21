5604d9b770ece3a8543c9feb771ce9f60dde2daf
Nicolas Boutin <nicolasb@theodo.fr>
2018-03-21 17:09:22 +0100

    Rename tel into phone to ensure compatibility with Symfony form_div_layout

--- DependencyInjection/Compiler/FormTwigTemplateCompilerPass.php
+++ DependencyInjection/Compiler/FormTwigTemplateCompilerPass.php
@@ -19,8 +19,8 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  */
 class FormTwigTemplateCompilerPass implements CompilerPassInterface
 {
-    private $telLayout = '@MisdPhoneNumber/Form/tel.html.twig';
-    private $telBootstrapLayout = '@MisdPhoneNumber/Form/tel_bootstrap.html.twig';
+    private $phoneLayout = '@MisdPhoneNumber/Form/phone.html.twig';
+    private $phoneBootstrapLayout = '@MisdPhoneNumber/Form/phone_bootstrap.html.twig';
 
     /**
      * {@inheritdoc}
@@ -33,20 +33,20 @@ class FormTwigTemplateCompilerPass implements CompilerPassInterface
 
         $parameter = $container->getParameter('twig.form.resources');
 
-        if (in_array($this->telLayout, $parameter)) {
+        if (in_array($this->phoneLayout, $parameter)) {
             return;
         }
 
         // Insert right after base template if it exists.
         if (($key = array_search('bootstrap_3_horizontal_layout.html.twig', $parameter)) !== false) {
-            array_splice($parameter, ++$key, 0, array($this->telBootstrapLayout));
+            array_splice($parameter, ++$key, 0, array($this->phoneBootstrapLayout));
         } elseif (($key = array_search('bootstrap_3_layout.html.twig', $parameter)) !== false) {
-            array_splice($parameter, ++$key, 0, array($this->telBootstrapLayout));
+            array_splice($parameter, ++$key, 0, array($this->phoneBootstrapLayout));
         } elseif (($key = array_search('form_div_layout.html.twig', $parameter)) !== false) {
-            array_splice($parameter, ++$key, 0, array($this->telLayout));
+            array_splice($parameter, ++$key, 0, array($this->phoneLayout));
         } else {
             // Put it in first position.
-            array_unshift($parameter, array($this->telLayout));
+            array_unshift($parameter, array($this->phoneLayout));
         }
 
         $container->setParameter('twig.form.resources', $parameter);
